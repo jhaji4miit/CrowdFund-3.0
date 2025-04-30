f267238d9552def3c889c16496a5cf4476ea84de
Vatsa <44364706+jhaji4miit@users.noreply.github.com>
2025-04-30 09:52:09 +0530

    Update service-worker.js

--- frontend/service-worker.js
+++ frontend/service-worker.js
@@ -1,21 +1,22 @@
-self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open('v1').then((cache) => {
-            return cache.addAll([
-                '/',
-                'index.html',
-                'style.css',
-                'app.js',
-                'assets/logo.png'
-            ]);
-        })
-    );
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open('crowdfund-cache').then(function (cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/style.css',
+        '/app.js',
+        '/manifest.json',
+        '/logo.png'
+      ]);
+    })
+  );
 });
 
-self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request).then((cachedResponse) => {
-            return cachedResponse || fetch(event.request);
-        })
-    );
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      return response || fetch(event.request);
+    })
+  );
 });
