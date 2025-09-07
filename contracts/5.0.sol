25002d6e61a38c59e6d96cc3187975a7edcb93d6
Vatsa <44364706+jhaji4miit@users.noreply.github.com>
2025-09-07 14:17:51 +0530

    total funds collection

--- contracts/5.0.sol
+++ contracts/5.0.sol
@@ -4,9 +4,14 @@ pragma solidity ^0.8.0;
 contract SimpleCrowdFund {
     mapping(address => uint) public contributions;
 
-    // Basic function: Accept contributions from anyone
+    // Accept contributions from anyone
     function contribute() external payable {
         require(msg.value > 0, "Contribution must be greater than zero");
         contributions[msg.sender] += msg.value;
+    }
+
+    // NEW FUNCTION: View total funds collected in the contract
+    function getTotalFunds() external view returns (uint) {
+        return address(this).balance;
     }
 }
